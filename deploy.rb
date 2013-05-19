b32a14a7c3991652683b31838bc4108ce1d556a9
codingfogey <codingfogey@gmail.com>
2013-05-19 19:59:04 +0200

    Update deploy.rb

--- deploy.rb
+++ deploy.rb
@@ -1,16 +1,16 @@
-set :application, "application"
-set :domain,      "domain"
+set :stages,        %w(production staging)
+set :default_stage, "staging"
+set :stage_dir,     "app/config"
+require 'capistrano/ext/multistage'
+
+set :application, "symfony-codingfogey"
 set :deploy_to,   "/var/www/#{application}"
 set :app_path,    "app"
 
-set :repository,  "#{domain}:/var/git/#{application}.git"
+set :repository,  "ssh://localhost/var/git/#{application}.git"
 set :scm,         :git
 
 set :model_manager, "doctrine"
-
-role :web,        domain                         # Your HTTP server, Apache/etc
-role :app,        domain                         # This may be the same as your `Web` server
-role :db,         domain, :primary => true       # This is where Symfony2 migrations will run
 
 set  :use_sudo,             false
 set  :keep_releases,        3
@@ -25,6 +25,7 @@ set :webserver_user,        "www-data"
 set :permission_method,     :acl
 set :use_set_permissions,   true
 set :user,                  "jenkins"
+set :group,                 "www-data"
 set :interactive_mode,      false
 set :clear_controllers,     false
 
